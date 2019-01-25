67ff8b320c4e205d6ab8037e6a167df901947417
Ben Manekin <ben.manekin@gmail.com>
2019-01-25 19:02:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,7 +2,12 @@ def instantiate_new_array
   new_array = Array.new
 end
 
+
 def array_with_two_elements
   instantiate_new_array
   new_array << "thing1"
-  
+  new_array << "thing2"
+  new_array
+end
+
+
